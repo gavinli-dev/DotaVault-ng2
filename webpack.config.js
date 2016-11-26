017a8fea8154e419f948dc63270f0141112bb815
gavinli-dev <gavin.li.dev@gmail.com>
2016-11-26 14:54:18 +1030

    remove unwanted files and add skeleton css

--- webpack.config.js
+++ webpack.config.js
@@ -5,25 +5,24 @@ var htmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
+    context: './assets',
     entry: {
+        //'assetsFolder': 'assets',
         'polyfills': './vendor/polyfill.ts',
         // 'vendor': './vendor/vendor.ts',
         'front': './front/main.ts',
         'admin': './admin/main.ts'
     },
-  output: {
-    path: ('./dist-hot'),
-    publicPath: 'http://localhost:8080/',
-    filename: '[name].js'
-    //chunkFilename: '[id].chunk.js'
-  },
-  resolve: {
+    output: {
+        path: ('./dist-hot'),
+        publicPath: 'http://localhost:8080/',
+        filename: '[name].js'
+        //chunkFilename: '[id].chunk.js'
+    },
+    resolve: {
         extensions: ['', '.ts', '.js']
     },
-  plugins: [
-    new ExtractTextPlugin('[name].css')
-  ],
-  module: {
+    module: {
         loaders: [
             {
                 test: /\.ts$/,
@@ -36,49 +35,46 @@ module.exports = {
             },
             {
                 test: /\.(jpg|png|gif|svg)$/i,
-                loader: 'file?name=assets/[name].[ext]'
+                loader: 'file?name=[path][name].[ext]&context=./assets'
             },
             {
                 test: /\.css$/,
                 loader: 'raw'
             }
         ]
-  },
-  plugins: [
-      new htmlPlugin({
-          filename: 'index.html',
-          template: 'front/index.html',
-          chunks: ['polyfills', 'front']
-      }),
-      new htmlPlugin({
-          filename: 'admin.html',
-          template: 'admin/index.html',
-          chunks: ['polyfills', 'admin']
-      })
-  ],
-  devServer: {
-    contentBase: './dist-hot',
-    // hot: true,
-    // inline: true,
-    // progress: true,
-    
-    //quiet: true,
-    stats: { colors: true },
-    proxy: {
-      "/api": {
-        target: {
-          "host": "localhost",
-          "protocal": "http",
-          "port": "8081"
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].css'),
+        new htmlPlugin({
+            filename: 'index.html',
+            template: 'front/index.html',
+            chunks: ['polyfills', 'front']
+        }),
+        new htmlPlugin({
+            filename: 'admin.html',
+            template: 'admin/index.html',
+            chunks: ['polyfills', 'admin']
+        })
+    ],
+    devServer: {
+        contentBase: './dist-hot',
+        stats: { colors: true },
+        //quiet: true,
+        //stats: 'errors-only',
+        proxy: {
+            "/api": {
+                target: {
+                    "host": "localhost",
+                    "protocal": "http",
+                    "port": "8081"
+                },
+                secure: false
+            }
         },
-        secure: false
-      }
-    },
-    historyApiFallback: {
-      rewrites: [
-        { from: /^\/admin/, to: '/admin.html' }
-      ]
-    },
-    //stats: 'errors-only',
-  }
+        historyApiFallback: {
+            rewrites: [
+                { from: /^\/admin/, to: '/admin.html' }
+            ]
+        }
+    }
 };
